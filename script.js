627fa820708a8697435b7c2c7b0f6e849d253df7
GabrielG865 <146103580+GabrielG865@users.noreply.github.com>
2025-02-13 17:24:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function moveRandom(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+
+}
+
+const moveRandomButton = document.querySelector
+("#move-random");
+
+moveRandomButton.addEventListener("mouseenter", function (e) {
+        moveRandom(e.target);
+    });
